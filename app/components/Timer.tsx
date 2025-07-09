b5ccab906a4a2311cdd85e7c24d8e04b78685dec
mathias1601 <mxyang@uio.no>
2025-07-09 23:12:17 +0200

    feat: add swappable timers with drag-and-drop

--- app/components/Timer.tsx
+++ app/components/Timer.tsx
@@ -1,15 +1,22 @@
 'use client'
 import React, { CSSProperties, useEffect, useRef, useState } from 'react'
 
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities"
+
 interface Props {
+	id: number,
 	time: number,
-	isActive: boolean, 
+	isActive: boolean,
 	onComplete: () => void, //Tells the TimerContainer when the timer has finished
-	name?: string, 
+	name?: string,
 	repetitions: number
 }
 
-const Timer = ({time, isActive, onComplete, name="", repetitions=1}: Props) => {
+const Timer = ({ id, time, isActive, onComplete, name = "", repetitions = 1 }: Props) => {
+
+	//For drag-n-drop
+	const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id })
 
 	const [remaining, setRemaining] = useState<number>(time)
 	const originalTime = time //The initial amount of time when component was created
@@ -36,87 +43,94 @@ const Timer = ({time, isActive, onComplete, name="", repetitions=1}: Props) => {
 	}, [alarmUrl])
 
 	const playAlarm = () => {
-    if (alarmRef.current) {
-      alarmRef.current.play();
-    }
-  }
+		if (alarmRef.current) {
+			alarmRef.current.play();
+		}
+	}
 
 	const stopAlarm = () => {
-    if (alarmRef.current) {
-      alarmRef.current.pause();
-      alarmRef.current.currentTime = 0;
-    }
-  }
+		if (alarmRef.current) {
+			alarmRef.current.pause();
+			alarmRef.current.currentTime = 0;
+		}
+	}
 
 	const minutes = Math.floor(remaining / 60);
-  const seconds = remaining % 60;
+	const seconds = remaining % 60;
 
 	useEffect(() => {
-    if (!isActive) return;
+		if (!isActive) return;
 
 		if (remaining <= 0 && repeat > 1) {
 			setRepeat(repeat - 1)
 			setRemaining(originalTime)
 		}
 
-    if (remaining <= 0 && repeat <= 1 && repeat > 0) {
+		if (remaining <= 0 && repeat <= 1 && repeat > 0) {
 			setRepeat(repeat - 1)
 			setDone(true)
 			stopAlarm()
 			playAlarm()
-      onComplete();
-      return;
-    }
+			onComplete();
+			return;
+		}
 
 		//Check this portion further
-    const interval = setInterval(() => {
-      setRemaining((prev) => {
-        if (prev <= 1) {
-          clearInterval(interval);
-          return 0;
-        }
-        return prev - 1;
-      });
-    }, 1000);
+		const interval = setInterval(() => {
+			setRemaining((prev) => {
+				if (prev <= 1) {
+					clearInterval(interval);
+					return 0;
+				}
+				return prev - 1;
+			});
+		}, 1000);
 
-    return () => clearInterval(interval);
-  }, [isActive, remaining]);
+		return () => clearInterval(interval);
+	}, [isActive, remaining]);
 
 	const timerStyleActive: CSSProperties = {
-      padding: '1rem',
-      borderRadius: '0.75rem',
-      boxShadow: '0 4px 12px rgba(0, 0, 0, 0.1)',
-      backgroundColor: 'white',
-      maxWidth: '250px',
-      margin: '1rem auto',
-      textAlign: 'center',
-  }
+		padding: '1rem',
+		borderRadius: '0.75rem',
+		boxShadow: '0 4px 12px rgba(0, 0, 0, 0.1)',
+		backgroundColor: 'white',
+		maxWidth: '250px',
+		margin: '1rem auto',
+		textAlign: 'center',
+	}
 
 	const timerStyleUnactive: CSSProperties = {
-      padding: '1rem',
-      borderRadius: '0.75rem',
-      boxShadow: '0 4px 12px rgba(0, 0, 0, 0.1)',
-      backgroundColor: 'red',
-      maxWidth: '250px',
-      margin: '1rem auto',
-      textAlign: 'center',
-  }
-	
+		padding: '1rem',
+		borderRadius: '0.75rem',
+		boxShadow: '0 4px 12px rgba(0, 0, 0, 0.1)',
+		backgroundColor: 'red',
+		maxWidth: '250px',
+		margin: '1rem auto',
+		textAlign: 'center',
+	}
+
 	const timerStyle = done ? timerStyleUnactive : timerStyleActive;
 
-  return (
+	const dragNDropStyle = {
+		transition,
+		transform: CSS.Transform.toString(transform),
+	}
+
+	return (
 		<>
-			<div style={timerStyle}>
-				<h2>{name}</h2>
-				<h2>
-					{`${minutes}: 
-						${seconds < 10 ? `0${seconds}`: seconds}
-					`}
-				</h2>
-				<p>X {repeat}</p>
+			<div ref={setNodeRef} {...attributes} {...listeners} style={dragNDropStyle}>
+				<div style={timerStyle}>
+					<h2>{name}</h2>
+					<h2>
+						{`${minutes}: 
+							${seconds < 10 ? `0${seconds}` : seconds}
+						`}
+					</h2>
+					<p>X {repeat}</p>
+				</div>
 			</div>
 		</>
-  )
+	)
 }
 
 export default Timer
--- app/components/TimerMenu.tsx
+++ app/components/TimerMenu.tsx
@@ -4,11 +4,14 @@ import '../styles/TimerStyles.css';
 import CONTAINER_PRESETS from '../TimerPresets/TimerPresets';
 import OverlayMenu from './OverlayMenu';
 
+import { SortableContext, verticalListSortingStrategy } from "@dnd-kit/sortable";
+import { closestCenter, closestCorners, DndContext } from '@dnd-kit/core';
+
 type TimerData = {
-  id: number;
-  duration: number;
-  name?: string;
-  hasFinished: boolean;
+	id: number;
+	duration: number;
+	name?: string;
+	hasFinished: boolean;
 	repetitions: number;
 
 	originalDuration: number;
@@ -52,64 +55,64 @@ const TimerMenu = () => {
 
 	//Function to change the name of a time-container
 	const changeName = (newName: string, id: number) => {
-		setContainerList(containerList => 
-			containerList.map(container => container.id === id ? {...container, name: newName} : container)
+		setContainerList(containerList =>
+			containerList.map(container => container.id === id ? { ...container, name: newName } : container)
 		)
 	}
 
-	const displayContainers = containerList.map((container, index) => (
-		(container.name != currentOpenContainer && openContainer) ? null
-		:
-		<div className='timeContainer' key={container.id}>
-			<TimerContainer
-				id={container.id}
-  			name={container.name}
-  			whenOpen={whenOpen}
-  			timers={container.timers}
-  			setTimers={(newTimers) => {
-    			setContainerList(prev =>
-      			prev.map(c =>
-        		c.name === container.name
-          	? { ...c, timers: newTimers }
-          	: c
-      			)
-    			);
-  			}}
-				changeName={changeName}
-			/>
-			<button onClick={() => {
-      	setContainerList(prev => prev.filter(t => t.id !== container.id));
-    	}}>
-      	Remove Container
-    	</button>
-		</div>
-  )); 
+	const displayContainers =
+		containerList.map((container) => (
+			(container.name != currentOpenContainer && openContainer) ? null
+				:
+				<div className='timeContainer' key={container.id}>
+					<TimerContainer
+						id={container.id}
+						name={container.name}
+						whenOpen={whenOpen}
+						timers={container.timers}
+						setTimers={(newTimers) => {
+							setContainerList(prev =>
+								prev.map(c =>
+									c.name === container.name
+										? { ...c, timers: newTimers }
+										: c
+								)
+							);
+						}}
+						changeName={changeName}
+					/>
+					<button onClick={() => {
+						setContainerList(prev => prev.filter(t => t.id !== container.id));
+					}}>
+						Remove Container
+					</button>
+				</div>
+		));
 
 	const addContainer = () => {
-		setContainerList(prev => [...prev, {id: Date.now(), name: containerName, timers: []}])	
+		setContainerList(prev => [...prev, { id: Date.now(), name: containerName, timers: [] }])
 	}
 
 	const addPreset = (preset: any) => {
 		if (!containerList.some(container => container.name == preset.name)) {
-			setContainerList(prev => [...prev, {id: Date.now(), name: preset.name, timers: preset.timers}])	
+			setContainerList(prev => [...prev, { id: Date.now(), name: preset.name, timers: preset.timers }])
 		}
 		else {
-			setContainerList(prev => [...prev, {id: Date.now(), name: preset.name + "(1)", timers: []}])
+			setContainerList(prev => [...prev, { id: Date.now(), name: preset.name + "(1)", timers: [] }])
 		}
 	}
 
-  return (
-    <div className='menuDiv'>
+	return (
+		<div className='menuDiv'>
 			<h3>Create a time-set</h3>
-			<input type="text" placeholder='name' onChange={(e) => setContainerName((e.target.value))}/>
+			<input type="text" placeholder='name' onChange={(e) => setContainerName((e.target.value))} />
 			<button onClick={addContainer}>Add container</button>
-			<OverlayMenu addPreset = {addPreset} listOfPresets = {presets}/>
+			<OverlayMenu addPreset={addPreset} listOfPresets={presets} />
 			<div>
 				{displayContainers}
 			</div>
-			
 		</div>
-  )
+	)
 }
 
 export default TimerMenu
